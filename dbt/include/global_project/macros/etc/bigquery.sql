c80aca26231125658c993800c7aa2259ec92ecc5
Drew Banin <drew@fishtownanalytics.com>
2018-02-02 23:05:19 -0500

    list of dates for bq dp tables

--- dbt/include/global_project/macros/etc/bigquery.sql
+++ dbt/include/global_project/macros/etc/bigquery.sql
@@ -2,27 +2,3 @@
 {% macro date_sharded_table(base_name) %}
     {{ return(base_name ~ "[DBT__PARTITION_DATE]") }}
 {% endmacro %}
-
-{% macro dates_in_range(start_date_str, end_date_str=none, date_fmt="%Y%m%d" %}
-    {% set end_date_str = start_date_str if end_date_str is none else end_date_str %}
-
-    {% set start_date = convert_datetime(start_date_str, fmt) %}
-    {% set end_date = convert_datetime(end_date_str, fmt) %}
-
-    {% set day_count = (end_date - start_date).days %}
-    {% if day_count < 0 %}
-        {% set msg -%}
-            Partiton start date is after the end date ({{ start_date }}, {{ end_date }})
-        {%- endset %}
-        {{ exceptions.raise_compiler_error(msg) }}
-    {% endif %}
-
-    [% set date_list = [] %}
-    {% for i in range(0, day_count + 1) %}
-        {% set the_date = (modules.datetime.timedelta(days=i) + start_date) %}
-        {% set _ = date_list.append(the_date) %}
-    {% endfor %}
-
-    {{ return(date_list) }}
-{% endmacro %}
-
--- dbt/include/global_project/macros/materializations/bigquery.sql
+++ dbt/include/global_project/macros/materializations/bigquery.sql
@@ -1,7 +1,6 @@
 {% materialization view, adapter='bigquery' -%}
 
   {%- set identifier = model['name'] -%}
-  {%- set tmp_identifier = identifier + '__dbt_tmp' -%}
   {%- set non_destructive_mode = (flags.NON_DESTRUCTIVE == True) -%}
   {%- set existing = adapter.query_for_existing(schema) -%}
   {%- set existing_type = existing.get(identifier) -%}
@@ -26,79 +25,52 @@
 {%- endmaterialization %}
 
 
-{% macro make_date_partitioned_table(model, partition_date, should_create, verbose=False) %}
-
-
-  {% set start_date = partition_date.start_date %}
-  {% set end_date = partition_date.end_date %}
+{% macro make_date_partitioned_table(model, dates, should_create, verbose=False) %}
 
   {% if should_create %}
       {{ adapter.make_date_partitioned_table(model.schema, model.name) }}
   {% endif %}
 
-  {% set day_count = (end_date - start_date).days %}
-
-  {% if day_count < 0 %}
-    {% set msg -%}
-        Partiton start date is after the end date ({{ start_date }}, {{ end_date }})
-    {%- endset %}
-    {{ exceptions.raise_compiler_error(msg, model) }}
-  {% endif %}
-
-  {% for i in range(0, day_count + 1) %}
-    {% set the_day = (modules.datetime.timedelta(days=i) + start_date).strftime('%Y%m%d') %}
+  {% for date in dates %}
+    {% set date = (date | string) %}
     {% if verbose %}
         {% set table_start_time = modules.datetime.datetime.now().strftime("%H:%M:%S") %}
-        {{ log(table_start_time ~ ' | -> Running for day ' ~ the_day, info=True) }}
+        {{ log(table_start_time ~ ' | -> Running for day ' ~ date, info=True) }}
     {% endif %}
 
-    {% set fixed_sql = model['injected_sql'] | replace('[DBT__PARTITION_DATE]', the_day) %}
-    {% set _ = adapter.execute_model(model, 'table', fixed_sql, decorator=the_day) %}
+    {% set fixed_sql = model['injected_sql'] | replace('[DBT__PARTITION_DATE]', date) %}
+    {% set _ = adapter.execute_model(model, 'table', fixed_sql, decorator=date) %}
   {% endfor %}
 
-  {% if day_count == 0 %}
+  {% set num_days = dates | length %}
+  {% if num_days == 1 %}
       {% set result_str = 'CREATED 1 PARTITION' %}
   {% else %}
-      {% set result_str = 'CREATED ' ~ (day_count + 1) ~ ' PARTITIONS' %}
+      {% set result_str = 'CREATED ' ~ num_days ~ ' PARTITIONS' %}
   {% endif %}
 
   {{ return(result_str) }}
 
 {% endmacro %}
 
-{% macro get_partition_date(raw_partition_date, date_fmt) %}
-
-    {% if not raw_partition_date %}
-      {{ return({"start_date": none, "end_date": none, "is_partitioned": false}) }}
-    {% endif %}
-
-    {% set partition_range = (raw_partition_date | string).split(",") %}
-
-    {% if (partition_range | length) == 1 %}
-      {% set start_date = convert_datetime(partition_range[0], date_fmt) %}
-      {% set end_date = start_date %}
-    {% elif (partition_range | length) == 2 %}
-      {% set start_date = convert_datetime(partition_range[0], date_fmt) %}
-      {% set end_date = convert_datetime(partition_range[1], date_fmt) %}
-    {% else %}
-      {{ dbt.exceptions.raise_compiler_error("Invalid partition time. Expected format: {Start Date}[,{End Date}]. Got: " ~ raw_partition_date) }}
-    {% endif %}
-
-    {{ return({"start_date": start_date, "end_date": end_date, "is_partitioned": true}) }}
-
-{% endmacro %}
-
 {% materialization table, adapter='bigquery' -%}
 
   {%- set identifier = model['name'] -%}
-  {%- set tmp_identifier = identifier + '__dbt_tmp' -%}
   {%- set non_destructive_mode = (flags.NON_DESTRUCTIVE == True) -%}
   {%- set existing = adapter.query_for_existing(schema) -%}
   {%- set existing_type = existing.get(identifier) -%}
-
   {%- set verbose = config.get('verbose', False) -%}
-  {%- set partition_date_fmt = config.get('partition_date_format') or '%Y%m%d' -%}
-  {%- set partition_date = get_partition_date(config.get('partition_date'), partition_date_fmt) -%}
+  {%- set partitions = config.get('partitions') -%}
+
+  {% if partitions %}
+      {% if partitions is number or partitions is string %}
+        {% set partitions = [(partitions | string)] %}
+      {% endif %}
+
+      {% if partitions is not iterable %}
+        {{ exceptions.raise_compiler_error("Provided `partitions` configuration is not a list. Got: " ~ partitions, model) }}
+      {% endif %}
+  {% endif %}
 
   {#
       Since dbt uses WRITE_TRUNCATE mode for tables, we only need to drop this thing
@@ -109,8 +81,8 @@
   {%- endif -%}
 
   -- build model
-  {% if partition_date.is_partitioned %}
-      {% set result = make_date_partitioned_table(model, partition_date, (existing_type != 'table'), verbose) %}
+  {% if partitions %}
+      {% set result = make_date_partitioned_table(model, partitions, (existing_type != 'table'), verbose) %}
   {% else %}
       {% set result = adapter.execute_model(model, 'table') %}
   {% endif %}
